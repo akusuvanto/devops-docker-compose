1bd6e604994558a46ef80d4daf8382efeacafa39
akusuvanto <41476300+akusuvanto@users.noreply.github.com>
2024-10-07 11:20:38 +0300

    Added getRunningProcesses

--- service2/app.go
+++ service2/app.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+type Process struct {
+	PID     string
+	COMMAND string
+}
+
 func getLocalIPAddresses() []string {
 	outBytes, err := exec.Command("bash", "-c", "hostname -I").Output()
 	if err != nil {
@@ -21,6 +26,34 @@ func getLocalIPAddresses() []string {
 	return addressArray
 }
 
+func getRunningProcesses() []Process {
+	outBytes, err := exec.Command("bash", "-c", "ps --no-headers -ax -o pid,command").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out := string(outBytes)
+	outTrimmed := strings.TrimSuffix(out, "\n")
+	processes := strings.Split(outTrimmed, "\n")
+	var processArray []Process
+
+	for _, line := range processes {
+		process := strings.TrimSuffix(line, "\n")
+		processTrimmed := strings.Trim(process, " ")
+
+		var firstSpace = strings.Index(processTrimmed, " ")
+
+		pid := processTrimmed[0:firstSpace]
+		command := processTrimmed[firstSpace:]
+
+		p := Process{pid, command}
+		processArray = append(processArray, p)
+	}
+	return processArray
+}
+
 func main() {
-	fmt.Printf("%v", getLocalIPAddresses())
+	//fmt.Printf("%v", getLocalIPAddresses())
+	getRunningProcesses()
+	fmt.Printf("%v", getRunningProcesses())
 }
