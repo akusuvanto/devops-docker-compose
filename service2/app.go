d9d524ca9d4f2d42cd11232135e8c0375cb45698
akusuvanto <41476300+akusuvanto@users.noreply.github.com>
2024-10-07 11:42:45 +0300

    Added getAvailableDiskSpace

--- service2/app.go
+++ service2/app.go
@@ -12,6 +12,13 @@ type Process struct {
 	COMMAND string
 }
 
+type Filesystem struct {
+	Filesystem string
+	Size       string
+	Available  string
+	Used       string
+}
+
 func getLocalIPAddresses() []string {
 	outBytes, err := exec.Command("bash", "-c", "hostname -I").Output()
 	if err != nil {
@@ -52,8 +59,41 @@ func getRunningProcesses() []Process {
 	return processArray
 }
 
+func getAvailableDiskSpace() []Filesystem {
+
+	outBytes, err := exec.Command("bash", "-c", "df -h --output=source,size,avail,pcent").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out := string(outBytes)
+	outTrimmed := strings.TrimSuffix(out, "\n")
+	filesystems := strings.Split(outTrimmed, "\n")
+
+	// remove the first element (headers)
+	var filesystemsTrimmed = append(filesystems[:0], filesystems[1:]...)
+
+	var fsArray []Filesystem
+
+	for _, line := range filesystemsTrimmed {
+		lineTrimmed := strings.TrimSuffix(line, "\n")
+
+		lineData := strings.Split(lineTrimmed, " ")
+
+		var lineDataClean []string
+		for _, str := range lineData {
+			if str != "" {
+				lineDataClean = append(lineDataClean, str)
+			}
+		}
+		f := Filesystem{lineDataClean[0], lineDataClean[1], lineDataClean[2], lineDataClean[3]}
+		fsArray = append(fsArray, f)
+	}
+	return fsArray
+}
+
 func main() {
 	//fmt.Printf("%v", getLocalIPAddresses())
-	getRunningProcesses()
-	fmt.Printf("%v", getRunningProcesses())
+	// getRunningProcesses()
+	fmt.Printf("%v", getAvailableDiskSpace())
 }
