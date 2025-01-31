f07aca495ce9348e9c48652ee787781e498bd2d3
akusuvanto <41476300+akusuvanto@users.noreply.github.com>
2025-01-31 18:07:32 +0200

    Add basic state changes without regards to special requirements

--- apigateway/apigateway.mjs
+++ apigateway/apigateway.mjs
@@ -6,11 +6,41 @@ let appState = 'INIT';
 // Set port by environment variable or default to 8000 for testing
 const port = process.env.PORT || 8000;
 const app = express();
+app.use(express.json())
 
 app.get('/state', (req, res) => {
     res.set('Content-Type', 'text/plain')
     res.send(appState)
 })
+
+// Responds to requests to change state via json payloads
+// Ex. using postman send a raw json body of {"status":"INIT"}
+app.put('/state', (req, res) => {
+    if (req.body.status == "INIT"){
+        appState = "INIT"
+        res.sendStatus(200)
+        return
+    }
+    if (req.body.status == "PAUSED"){
+        appState = "PAUSED"
+        res.sendStatus(200)
+        return
+    }
+    if (req.body.status == "RUNNING"){
+        appState = "RUNNING"
+        res.sendStatus(200)
+        return
+    }
+    if (req.body.status == "SHUTDOWN"){
+        appState = "SHUTDOWN"
+        res.sendStatus(200)
+        return
+    }
+    // None matched, bad request
+    res.sendStatus(400)
+    return
+})
+
 
 app.listen(port, () => {
     console.log(`API Gateway listening on port ${port}`)
