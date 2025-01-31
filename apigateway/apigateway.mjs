2f79f1a18670d995e0948987e8bb4bd86fcbb312
akusuvanto <41476300+akusuvanto@users.noreply.github.com>
2025-01-31 16:10:41 +0200

    API Gateway should return INIT State

--- apigateway/apigateway.mjs
+++ apigateway/apigateway.mjs
@@ -1,6 +1,17 @@
-import http from 'http';
+import express from 'express'
 
-http.createServer(function (req, res) {
-    res.write('API Gateway, Hello!');
-    res.end();
-}).listen(process.env.PORT);
+// Initialize the state to "INIT"
+let appState = 'INIT';
+
+// Set port by environment variable or default to 8000 for testing
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.get('/state', (req, res) => {
+    res.set('Content-Type', 'text/plain')
+    res.send(appState)
+})
+
+app.listen(port, () => {
+    console.log(`API Gateway listening on port ${port}`)
+})
