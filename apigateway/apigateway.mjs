a72ce8d1b52c8eda8ede0eee8586c238dacd3026
akusuvanto <41476300+akusuvanto@users.noreply.github.com>
2025-01-31 18:26:33 +0200

    Added Run-Log functionality

--- apigateway/apigateway.mjs
+++ apigateway/apigateway.mjs
@@ -2,6 +2,18 @@ import express from 'express'
 
 // Initialize the state to "INIT"
 let appState = 'INIT';
+let runLog = [];
+
+function changeState(newState){
+    // Nothing happens and no change is logged if state is not changed
+    if (newState == appState){
+        return;
+    }
+    let date = new Date().toJSON().toString();
+    runLog.push((date + ": " + appState + "->" + newState).toString());
+    appState = newState;
+    return;
+}
 
 // Set port by environment variable or default to 8000 for testing
 const port = process.env.PORT || 8000;
@@ -13,26 +25,32 @@ app.get('/state', (req, res) => {
     res.send(appState)
 })
 
+app.get('/run-log', (req, res) => {
+    res.set('Content-Type', 'text/plain')
+    let response = runLog.join("\n")
+    res.send(response);
+})
+
 // Responds to requests to change state via json payloads
 // Ex. using postman send a raw json body of {"status":"INIT"}
 app.put('/state', (req, res) => {
     if (req.body.status == "INIT"){
-        appState = "INIT"
+        changeState("INIT")
         res.sendStatus(200)
         return
     }
     if (req.body.status == "PAUSED"){
-        appState = "PAUSED"
+        changeState("PAUSED")
         res.sendStatus(200)
         return
     }
     if (req.body.status == "RUNNING"){
-        appState = "RUNNING"
+        changeState("RUNNING")
         res.sendStatus(200)
         return
     }
     if (req.body.status == "SHUTDOWN"){
-        appState = "SHUTDOWN"
+        changeState("SHUTDOWN")
         res.sendStatus(200)
         return
     }
